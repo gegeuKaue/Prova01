7d65f950291f8439e59ca57280ba2050c25c7570
geovane.santos <geovane.santos@CONTW-DES78.CONTMATIC.AD>
2019-08-01 08:57:06 -0300

    Salvando o projeto

---
+++ Prova1/src/main/java/br/com/geovane/prova/Prova1/App.java
@@ -0,0 +1,13 @@
+package br.com.geovane.prova.Prova1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
