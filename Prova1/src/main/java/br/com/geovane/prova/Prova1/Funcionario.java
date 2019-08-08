008af3dd80df99c3dafb552d1d06b446e5dca12b
geovane.santos <geovane.santos@CONTW-DES78.CONTMATIC.AD>
2019-08-08 13:58:35 -0300

    4.2.0

--- Prova1/src/main/java/br/com/geovane/prova/Prova1/Funcionario.java
+++ Prova1/src/main/java/br/com/geovane/prova/Prova1/Funcionario.java
@@ -3,6 +3,10 @@ package br.com.geovane.prova.Prova1;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class Funcionario.
+ */
 public class Funcionario {
     /** The nome. */
     private String nome;
@@ -19,28 +23,59 @@ public class Funcionario {
     /** The horario saida. */
     private double horarioSaida;
 
+    /** The data contratacao. */
     private Calendar dataContratacao;
 
+    /**
+     * Gets the nome.
+     *
+     * @return the nome
+     */
     public String getNome() {
         return nome;
     }
 
+    /**
+     * Sets the nome.
+     *
+     * @param nome the new nome
+     */
     public void setNome(String nome) {
         this.nome = nome;
     }
 
+    /**
+     * Gets the cargo.
+     *
+     * @return the cargo
+     */
     public String getCargo() {
         return cargo;
     }
 
+    /**
+     * Sets the cargo.
+     *
+     * @param cargo the new cargo
+     */
     public void setCargo(String cargo) {
         this.cargo = cargo;
     }
 
+    /**
+     * Gets the idade.
+     *
+     * @return the idade
+     */
     public int getIdade() {
         return idade;
     }
 
+    /**
+     * Sets the idade.
+     *
+     * @param idade the new idade
+     */
     public void setIdade(int idade) {
         if(idade > 0) {
             this.idade = idade;
@@ -49,6 +84,11 @@ public class Funcionario {
         
     }
 
+    /**
+     * Gets the horario entrada.
+     *
+     * @return the horario entrada
+     */
     public double getHorarioEntrada() {
         return horarioEntrada;
     }
@@ -65,6 +105,11 @@ public class Funcionario {
             throw new IllegalArgumentException("O horário deve ser entre 0h a 24h");
     }
 
+    /**
+     * Gets the horario saida.
+     *
+     * @return the horario saida
+     */
     public double getHorarioSaida() {
         return horarioSaida;
     }
@@ -81,6 +126,16 @@ public class Funcionario {
             throw new IllegalArgumentException("O horário deve ser entre 0h a 24h");
     }
 
+    /**
+     * Instantiates a new funcionario.
+     *
+     * @param nome the nome
+     * @param cargo the cargo
+     * @param idade the idade
+     * @param horarioEntrada the horario entrada
+     * @param horarioSaida the horario saida
+     * @param dataContratacao the data contratacao
+     */
     public Funcionario(String nome, String cargo, int idade, double horarioEntrada, double horarioSaida, Calendar dataContratacao) {
         super();
         this.nome = nome;
@@ -91,6 +146,11 @@ public class Funcionario {
         this.dataContratacao = dataContratacao;
     }
 
+    /**
+     * Hash code.
+     *
+     * @return the int
+     */
     @Override
     public int hashCode() {
         final int prime = 31;
@@ -107,6 +167,12 @@ public class Funcionario {
         return result;
     }
 
+    /**
+     * Equals.
+     *
+     * @param obj the obj
+     * @return true, if successful
+     */
     @Override
     public boolean equals(Object obj) {
         if (this == obj)
@@ -178,10 +244,20 @@ public class Funcionario {
         return builder.toString();
     }
 
+    /**
+     * Gets the data contratacao.
+     *
+     * @return the data contratacao
+     */
     public Calendar getDataContratacao() {
         return dataContratacao;
     }
 
+    /**
+     * Sets the data contratacao.
+     *
+     * @param dataContratacao the new data contratacao
+     */
     public void setDataContratacao(Calendar dataContratacao) {
         this.dataContratacao = dataContratacao;
     }
