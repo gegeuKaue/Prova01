9c790d9a28644e4893bda8f3f25f073e6d5f3245
gegeuKaue <geovane.kaue.gk@gmail.com>
2019-08-14 10:04:17 -0300

    Space

--- Prova1/src/main/java/br/com/geovane/prova/Prova1/Acao.java
+++ Prova1/src/main/java/br/com/geovane/prova/Prova1/Acao.java
@@ -5,16 +5,16 @@ package br.com.geovane.prova.Prova1;
  * The Enum Acao.
  */
 public enum Acao {
-                  
+
                   /** The cadastrar funcionario. */
                   CADASTRAR_FUNCIONARIO,
-                  
+
                   /** The info todos funcionario. */
                   INFO_TODOS_FUNCIONARIO,
-                  
+
                   /** The info empresa. */
                   INFO_EMPRESA,
-                  
+
                   /** The sair. */
                   SAIR;
 }
