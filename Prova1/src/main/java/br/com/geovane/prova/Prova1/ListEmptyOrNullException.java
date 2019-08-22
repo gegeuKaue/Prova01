7d0d95892d04634c42e8310ca76434ffcbbb4ab0
gegeuKaue <geovane.kaue.gk@gmail.com>
2019-08-22 09:00:27 -0300

    Atualização para a prova 3.

---
+++ Prova1/src/main/java/br/com/geovane/prova/Prova1/ListEmptyOrNullException.java
@@ -0,0 +1,21 @@
+package br.com.geovane.prova.Prova1;
+
+// TODO: Auto-generated Javadoc
+/**
+ * The Class ListEmptyOrNullException.
+ */
+public class ListEmptyOrNullException extends RuntimeException {
+
+    /**
+     * Instantiates a new list empty or null exception.
+     *
+     * @param string the string
+     */
+    public ListEmptyOrNullException(String string) {
+        super(string);
+    }
+
+    /** The Constant serialVersionUID. */
+    private static final long serialVersionUID = 1L;
+    
+}
